5a53416d06268d028ed685ba9e0fd8497f86fd15
VitoriaLentz <vitoria.lentz@hotmail.com>
2024-07-17 23:20:04 -0300

    fix: update window

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,11 @@
 #include "utils.h"
 #include "matrices.h"
 
+float g_ScreenRatio = 1.0f;
+
+void ErrorCallback(int error, const char* description);
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+
 int main(int argc, char* argv[])
 {
     int success = glfwInit();
@@ -71,3 +76,29 @@ int main(int argc, char* argv[])
     return 0;
 }
 
+// Definimos o callback para impressão de erros da GLFW no terminal
+void ErrorCallback(int error, const char* description)
+{
+    fprintf(stderr, "ERROR: GLFW: %s\n", description);
+}
+
+// Definição da função que será chamada sempre que a janela do sistema
+// operacional for redimensionada, por consequência alterando o tamanho do
+// "framebuffer" (região de memória onde são armazenados os pixels da imagem).
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    // Indicamos que queremos renderizar em toda região do framebuffer. A
+    // função "glViewport" define o mapeamento das "normalized device
+    // coordinates" (NDC) para "pixel coordinates".  Essa é a operação de
+    // "Screen Mapping" ou "Viewport Mapping" vista em aula ({+ViewportMapping2+}).
+    glViewport(0, 0, width, height);
+
+    // Atualizamos também a razão que define a proporção da janela (largura /
+    // altura), a qual será utilizada na definição das matrizes de projeção,
+    // tal que não ocorra distorções durante o processo de "Screen Mapping"
+    // acima, quando NDC é mapeado para coordenadas de pixels. Veja slides 205-215 do documento Aula_09_Projecoes.pdf.
+    //
+    // O cast para float é necessário pois números inteiros são arredondados ao
+    // serem divididos!
+    g_ScreenRatio = (float)width / height;
+}
