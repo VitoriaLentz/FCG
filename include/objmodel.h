31b46f3d9b802d10a898b1ceb520951d035390e3
VitoriaLentz <vitoria.lentz@hotmail.com>
2024-08-18 09:12:34 -0300

    feat: add object model in another file

---
+++ include/objmodel.h
@@ -0,0 +1,45 @@
+#ifndef OBJMODEL_H_
+#define OBJMODEL_H_
+
+#include "sceneobject.h"
+#include "camera.h"
+#include "tiny_obj_loader.h"
+#include <string>
+#include "glm/vec4.hpp"
+
+class Obj {
+    private:
+
+    public:
+        tinyobj::attrib_t                 attrib;
+        std::vector<tinyobj::shape_t>     shapes;
+        std::vector<tinyobj::material_t>  materials;
+        Obj(const char* filename, const char* basepath = NULL, bool triangulate = true);
+        Obj();
+};
+
+class ObjModel {
+    public:
+        glm::vec3 scale;
+        std::string name;
+        int objectId;
+        glm::vec3 position;
+        glm::vec3 direction;
+        float rotation;
+        glm::vec3 originalPosition;
+        Obj obj;
+
+        void BuildTrianglesAndAddToVirtualScene(std::map<std::string, SceneObject> &virtualScene); // Constrói representação de um ObjModel como malha de triângulos para renderização
+        void ComputeNormals(); // Computa normais de um ObjModel, caso não existam.
+        
+        ObjModel(int id, glm::vec3 position, glm::vec3 scale, glm::vec3 direction, float rotation, const char* name, const char* path, std::map<std::string, SceneObject> &virtualScene);
+        float x_difference;
+        float z_difference;
+        glm::vec3 bbox_min;
+        glm::vec3 bbox_max;
+        void updateBbox();
+
+};
+
+
+#endif //OBJMODEL_H_
---
+++ src/objmodel.cpp
@@ -0,0 +1,279 @@
+#include "../include/objmodel.h"
+#include "../include/collisions.h"
+#include "glad/glad.h"
+
+Obj::Obj(){
+
+}
+
+Obj::Obj(const char* filename, const char* basepath, bool triangulate)
+{
+    printf("Carregando objetos do arquivo \"%s\"...\n", filename);
+
+    // Se basepath == NULL, então setamos basepath como o dirname do
+    // filename, para que os arquivos MTL sejam corretamente carregados caso
+    // estejam no mesmo diretório dos arquivos OBJ.
+    std::string fullpath(filename);
+    std::string dirname;
+    if (basepath == NULL)
+    {
+        auto i = fullpath.find_last_of("/");
+        if (i != std::string::npos)
+        {
+            dirname = fullpath.substr(0, i+1);
+            basepath = dirname.c_str();
+        }
+    }
+
+    std::string warn;
+    std::string err;
+    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename, basepath, triangulate);
+
+    if (!err.empty())
+        fprintf(stderr, "\n%s\n", err.c_str());
+
+    if (!ret)
+        throw std::runtime_error("Erro ao carregar modelo.");
+
+    for (size_t shape = 0; shape < shapes.size(); ++shape)
+    {
+        if (shapes[shape].name.empty())
+        {
+            fprintf(stderr,
+                    "*********************************************\n"
+                    "Erro: Objeto sem nome dentro do arquivo '%s'.\n"
+                    "Veja https://www.inf.ufrgs.br/~eslgastal/fcg-faq-etc.html#Modelos-3D-no-formato-OBJ .\n"
+                    "*********************************************\n",
+                filename);
+            throw std::runtime_error("Objeto sem nome.");
+        }
+        printf("- Objeto '%s'\n", shapes[shape].name.c_str());
+    }
+
+    printf("OK.\n");
+}
+
+ObjModel::ObjModel(int id, glm::vec3 position, glm::vec3 scale, glm::vec3 direction, float rotation, const char* name, const char* path, std::map<std::string, SceneObject> &virtualScene) {
+    this->objectId = id;
+    this->position = position;
+    this->originalPosition = position;
+    this->scale = scale;
+    this->direction = normalize(direction);
+    this->rotation = rotation;
+    this->name = name;
+    this->obj = Obj(path);
+    this->ComputeNormals();
+    this->BuildTrianglesAndAddToVirtualScene(virtualScene);
+}
+
+// Função que computa as normais de um ObjModel, caso elas não tenham sido
+// especificadas dentro do arquivo ".obj"
+void ObjModel::ComputeNormals()
+{
+    if ( !(this->obj).attrib.normals.empty() )
+        return;
+
+    // Primeiro computamos as normais para todos os TRIÂNGULOS.
+    // Segundo, computamos as normais dos VÉRTICES através do método proposto
+    // por Gouraud, onde a normal de cada vértice vai ser a média das normais de
+    // todas as faces que compartilham este vértice.
+
+    size_t num_vertices = (this->obj).attrib.vertices.size() / 3;
+
+    std::vector<int> num_triangles_per_vertex(num_vertices, 0);
+    std::vector<glm::vec4> vertex_normals(num_vertices, glm::vec4(0.0f,0.0f,0.0f,0.0f));
+
+    for (size_t shape = 0; shape < (this->obj).shapes.size(); ++shape)
+    {
+        size_t num_triangles = (this->obj).shapes[shape].mesh.num_face_vertices.size();
+
+        for (size_t triangle = 0; triangle < num_triangles; ++triangle)
+        {
+            assert((this->obj).shapes[shape].mesh.num_face_vertices[triangle] == 3);
+
+            glm::vec4  vertices[3];
+            for (size_t vertex = 0; vertex < 3; ++vertex)
+            {
+                tinyobj::index_t idx = (this->obj).shapes[shape].mesh.indices[3*triangle + vertex];
+                const float vx = (this->obj).attrib.vertices[3*idx.vertex_index + 0];
+                const float vy = (this->obj).attrib.vertices[3*idx.vertex_index + 1];
+                const float vz = (this->obj).attrib.vertices[3*idx.vertex_index + 2];
+                vertices[vertex] = glm::vec4(vx,vy,vz,1.0);
+            }
+
+            const glm::vec4  a = vertices[0];
+            const glm::vec4  b = vertices[1];
+            const glm::vec4  c = vertices[2];
+
+            const glm::vec4  n = crossproduct(b-a,c-a);
+
+            for (size_t vertex = 0; vertex < 3; ++vertex)
+            {
+                tinyobj::index_t idx = (this->obj).shapes[shape].mesh.indices[3*triangle + vertex];
+                num_triangles_per_vertex[idx.vertex_index] += 1;
+                vertex_normals[idx.vertex_index] += n;
+                (this->obj).shapes[shape].mesh.indices[3*triangle + vertex].normal_index = idx.vertex_index;
+            }
+        }
+    }
+
+    (this->obj).attrib.normals.resize( 3*num_vertices );
+
+    for (size_t i = 0; i < vertex_normals.size(); ++i)
+    {
+        glm::vec4 n = vertex_normals[i] / (float)num_triangles_per_vertex[i];
+        n /= norm(n);
+        (this->obj).attrib.normals[3*i + 0] = n.x;
+        (this->obj).attrib.normals[3*i + 1] = n.y;
+        (this->obj).attrib.normals[3*i + 2] = n.z;
+    }
+}
+
+// Constrói triângulos para futura renderização a partir de um ObjModel.
+void ObjModel::BuildTrianglesAndAddToVirtualScene(std::map<std::string, SceneObject> &virtualScene)
+{
+    GLuint vertex_array_object_id;
+    glGenVertexArrays(1, &vertex_array_object_id);
+    glBindVertexArray(vertex_array_object_id);
+
+    std::vector<GLuint> indices;
+    std::vector<float>  model_coefficients;
+    std::vector<float>  normal_coefficients;
+    std::vector<float>  texture_coefficients;
+
+    for (size_t shape = 0; shape < (this->obj).shapes.size(); ++shape)
+    {
+        size_t first_index = indices.size();
+        size_t num_triangles = (this->obj).shapes[shape].mesh.num_face_vertices.size();
+
+        const float minval = std::numeric_limits<float>::min();
+        const float maxval = std::numeric_limits<float>::max();
+
+        glm::vec3 bbox_min = glm::vec3(maxval,maxval,maxval);
+        glm::vec3 bbox_max = glm::vec3(minval,minval,minval);
+
+        for (size_t triangle = 0; triangle < num_triangles; ++triangle)
+        {
+            assert((this->obj).shapes[shape].mesh.num_face_vertices[triangle] == 3);
+
+            for (size_t vertex = 0; vertex < 3; ++vertex)
+            {
+                tinyobj::index_t idx = (this->obj).shapes[shape].mesh.indices[3*triangle + vertex];
+
+                indices.push_back(first_index + 3*triangle + vertex);
+
+                const float vx = (this->obj).attrib.vertices[3*idx.vertex_index + 0];
+                const float vy = (this->obj).attrib.vertices[3*idx.vertex_index + 1];
+                const float vz = (this->obj).attrib.vertices[3*idx.vertex_index + 2];
+                //printf("tri %d vert %d = (%.2f, %.2f, %.2f)\n", (int)triangle, (int)vertex, vx, vy, vz);
+                model_coefficients.push_back( vx ); // X
+                model_coefficients.push_back( vy ); // Y
+                model_coefficients.push_back( vz ); // Z
+                model_coefficients.push_back( 1.0f ); // W
+
+                bbox_min.x = std::min(bbox_min.x, vx);
+                bbox_min.y = std::min(bbox_min.y, vy);
+                bbox_min.z = std::min(bbox_min.z, vz);
+                bbox_max.x = std::max(bbox_max.x, vx);
+                bbox_max.y = std::max(bbox_max.y, vy);
+                bbox_max.z = std::max(bbox_max.z, vz);
+
+                // Inspecionando o código da tinyobjloader, o aluno Bernardo
+                // Sulzbach (2017/1) apontou que a maneira correta de testar se
+                // existem normais e coordenadas de textura no ObjModel é
+                // comparando se o índice retornado é -1. Fazemos isso abaixo.
+
+                if ( idx.normal_index != -1 )
+                {
+                    const float nx = (this->obj).attrib.normals[3*idx.normal_index + 0];
+                    const float ny = (this->obj).attrib.normals[3*idx.normal_index + 1];
+                    const float nz = (this->obj).attrib.normals[3*idx.normal_index + 2];
+                    normal_coefficients.push_back( nx ); // X
+                    normal_coefficients.push_back( ny ); // Y
+                    normal_coefficients.push_back( nz ); // Z
+                    normal_coefficients.push_back( 0.0f ); // W
+                }
+
+                if ( idx.texcoord_index != -1 )
+                {
+                    const float u = (this->obj).attrib.texcoords[2*idx.texcoord_index + 0];
+                    const float v = (this->obj).attrib.texcoords[2*idx.texcoord_index + 1];
+                    texture_coefficients.push_back( u );
+                    texture_coefficients.push_back( v );
+                }
+            }
+        }
+
+        size_t last_index = indices.size() - 1;
+
+        SceneObject theobject;
+        theobject.name           = (this->obj).shapes[shape].name;
+        theobject.first_index    = first_index; // Primeiro índice
+        theobject.num_indices    = last_index - first_index + 1; // Número de indices
+        theobject.rendering_mode = GL_TRIANGLES;       // Índices correspondem ao tipo de rasterização GL_TRIANGLES.
+        theobject.vertex_array_object_id = vertex_array_object_id;
+
+        theobject.bbox_min = bbox_min;
+        theobject.bbox_max = bbox_max;
+
+        virtualScene[(this->obj).shapes[shape].name] = theobject;
+    }
+
+    GLuint VBO_model_coefficients_id;
+    glGenBuffers(1, &VBO_model_coefficients_id);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_model_coefficients_id);
+    glBufferData(GL_ARRAY_BUFFER, model_coefficients.size() * sizeof(float), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, model_coefficients.size() * sizeof(float), model_coefficients.data());
+    GLuint location = 0; // "(location = 0)" em "shader_vertex.glsl"
+    GLint  number_of_dimensions = 4; // vec4 em "shader_vertex.glsl"
+    glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(location);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    if ( !normal_coefficients.empty() )
+    {
+        GLuint VBO_normal_coefficients_id;
+        glGenBuffers(1, &VBO_normal_coefficients_id);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_normal_coefficients_id);
+        glBufferData(GL_ARRAY_BUFFER, normal_coefficients.size() * sizeof(float), NULL, GL_STATIC_DRAW);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, normal_coefficients.size() * sizeof(float), normal_coefficients.data());
+        location = 1; // "(location = 1)" em "shader_vertex.glsl"
+        number_of_dimensions = 4; // vec4 em "shader_vertex.glsl"
+        glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(location);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    if ( !texture_coefficients.empty() )
+    {
+        GLuint VBO_texture_coefficients_id;
+        glGenBuffers(1, &VBO_texture_coefficients_id);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_texture_coefficients_id);
+        glBufferData(GL_ARRAY_BUFFER, texture_coefficients.size() * sizeof(float), NULL, GL_STATIC_DRAW);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, texture_coefficients.size() * sizeof(float), texture_coefficients.data());
+        location = 2; // "(location = 1)" em "shader_vertex.glsl"
+        number_of_dimensions = 2; // vec2 em "shader_vertex.glsl"
+        glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(location);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    GLuint indices_id;
+    glGenBuffers(1, &indices_id);
+
+    // "Ligamos" o buffer. Note que o tipo agora é GL_ELEMENT_ARRAY_BUFFER.
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_id);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, indices.size() * sizeof(GLuint), indices.data());
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // XXX Errado!
+    //
+
+    // "Desligamos" o VAO, evitando assim que operações posteriores venham a
+    // alterar o mesmo. Isso evita bugs.
+    glBindVertexArray(0);
+}
+
+void ObjModel::updateBbox(){
+    this->bbox_max = glm::vec3(this->position.x + this->x_difference, this->position.y, this->position.z + this->z_difference);
+    this->bbox_min = glm::vec3(this->position.x - this->x_difference, this->position.y, this->position.z - this->z_difference);
+}
